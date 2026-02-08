ccec70d693eb4a708371d504750f4d7fbc133cfb
Jose-Prince <joseprince148@gmail.com>
2026-02-08 17:41:04 -0600

    Feat: sound ice cracking

--- Assets/Scripts/IceTileController.cs
+++ Assets/Scripts/IceTileController.cs
@@ -7,12 +7,15 @@ public class IceTileController : MonoBehaviour
     [SerializeField] Tile iceTile;
     [SerializeField] Tile snowTile;
     [SerializeField] LevelManager levelManager;
+    [SerializeField] AudioClip iceClip;
 
     private Vector3Int lastcellPos;
     private bool tileChangeLocked = false;
 
     public void HandlePlayerStep(Vector3 worldFeetPosition)
     {
+        if (iceTilemap == null) return;
+
         Vector3Int cellPos = iceTilemap.WorldToCell(worldFeetPosition);
 
         if (cellPos == lastcellPos) return;
@@ -31,12 +34,19 @@ public class IceTileController : MonoBehaviour
         if (currentTile == iceTile)
         {
             iceTilemap.SetTile(cellPos, snowTile);
-            levelManager.UpdateTilesChanged();
+
+            if (iceClip != null && AudioManager.Instance != null)
+                AudioManager.Instance.PlaySFX(iceClip);
+
+            if (levelManager != null)
+                levelManager.UpdateTilesChanged();
         }
     }
 
     public void ResetIceTiles()
     {
+        if (iceTilemap == null) return;
+
         BoundsInt bounds = iceTilemap.cellBounds;
 
         foreach (Vector3Int pos in bounds.allPositionsWithin)
