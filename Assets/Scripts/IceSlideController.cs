85c0d615bb39296eb81323fc7a7dea3ac0233ed1
Jose-Prince <joseprince148@gmail.com>
2026-02-08 17:58:36 -0600

    Feat: ice sliding sound

--- Assets/Scripts/IceSlideController.cs
+++ Assets/Scripts/IceSlideController.cs
@@ -4,15 +4,47 @@ using UnityEngine.Tilemaps;
 public class IceSlideController : MonoBehaviour
 {
     [SerializeField] Tilemap iceSlipTilemap;
+    [SerializeField] private AudioClip slideClip;
+
+    private AudioSource audioSource;
+    private bool wasSlidingLastFrame = false;
+
+    void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = slideClip;
+        audioSource.loop = true;
+        audioSource.playOnAwake = false;
+        audioSource.spatialBlend = 0f; // 2D
+        audioSource.volume = 0.8f;
+    }
 
     public bool isSliding(Vector3 feetWorldPos, Vector3 inputDir)
     {
+        if (iceSlipTilemap == null) return false;
+
         Vector3Int cellPos = iceSlipTilemap.WorldToCell(feetWorldPos);
         TileBase iceTile = iceSlipTilemap.GetTile(cellPos);
 
-        if (iceTile != null && inputDir != Vector3.zero)
-            return true;
+        bool slidingNow = (iceTile != null && inputDir != Vector3.zero);
 
-        return false;
+        HandleSlideSound(slidingNow);
+
+        return slidingNow;
+    }
+
+    private void HandleSlideSound(bool slidingNow)
+    {
+        if (slidingNow && !wasSlidingLastFrame)
+        {
+            if (slideClip != null)
+                audioSource.Play();
+        }
+        else if (!slidingNow && wasSlidingLastFrame)
+        {
+            audioSource.Stop();
+        }
+
+        wasSlidingLastFrame = slidingNow;
     }
 }
